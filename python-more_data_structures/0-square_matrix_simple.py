6b022cde5a0cd3ebc194463134c63289d185966d
Elbe81 <luiselberivera@gmail.com>
2024-02-01 14:49:10 -0400

    2 dimentional array

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+
+    new_matrix = []
+    for row in matrix:
+        new_row = []
+        for num in row:
+            new_row.append(num ** 2)
+        new_matrix.append(new_row)
+    return new_matrix
+
+if __name__ == "__main__":
+    square_matrix_simple = __import__('0-square_matrix_simple').square_matrix_simple
+
+    matrix = [
+            [1, 2, 3],
+            [4, 5, 6],
+            [7, 8, 9]
+            ]
+
+    new_matrix = square_matrix_simple(matrix)
+    print(new_matrix)
+    print(matrix)
