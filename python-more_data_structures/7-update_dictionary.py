dab1ace7d63a1cc10688553b1bb418c646d903f0
Elbe81 <luiselberivera@gmail.com>
2024-02-02 12:21:35 -0400

    Update dic

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 
-def print_sorted_dictionary(a_dictionary):
-    for k, v in (sorted(a_dictionary.items())):
-        print("{}: {}".format(k, v))
+def update_dictionary(a_dictionary, key, value):
+    for k, v in a_dictionary.items():
+        if k == key:
+            a_dictionary[k] = value
+            break
+    else:
+        a_dictionary[key] = value
+    return (a_dictionary)
